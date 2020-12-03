ed91b94f8e199c84d827cb5f740e417d7f3acdb3
Peter Tamas Galik <galikpetertamas@gmail.com>
2020-12-03 07:53:17 +0100

    Initial commit

---
+++ src/main/java/com/galikpeter11/librarion9000/Librarion9000UltraProMaxApplication.java
@@ -0,0 +1,13 @@
+package com.galikpeter11.librarion9000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Librarion9000UltraProMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Librarion9000UltraProMaxApplication.class, args);
+	}
+
+}
