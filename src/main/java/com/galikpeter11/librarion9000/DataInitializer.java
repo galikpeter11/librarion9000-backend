0fbf3ad0ec426e354bbf245bf9c3e527b357e20f
Peter Tamas Galik <galikpetertamas@gmail.com>
2020-12-03 17:03:38 +0100

    Minor code cleanup

--- src/main/java/com/galikpeter11/librarion9000/DataInitializer.java
+++ src/main/java/com/galikpeter11/librarion9000/DataInitializer.java
@@ -12,7 +12,8 @@ import java.util.List;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
-    final Logger log = LoggerFactory.getLogger(getClass());
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
     private final BookRepository bookRepository;
 
     public DataInitializer(BookRepository bookRepository) {
@@ -23,14 +24,14 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args) {
         List<Book> books = new ArrayList<>();
 
-        books.add(new Book(1L, "The Hunger Games", 2008, 374, "Lorem Ipsum..."));
-        books.add(new Book(2L, "Harry Potter and the Order of the Phoenix", 2004, 870, "Lorem Ipsum..."));
-        books.add(new Book(3L, "To Kill a Mockingbird", 2006, 324, "Lorem Ipsum..."));
-        books.add(new Book(4L, "Pride and Prejudice", 2000, 279, "Lorem Ipsum..."));
-        books.add(new Book(5L, "The Book Thief", 2006, 552, "Lorem Ipsum..."));
+        books.add(new Book("The Hunger Games", 2008, 374, "Lorem Ipsum..."));
+        books.add(new Book("Harry Potter and the Order of the Phoenix", 2004, 870, "Lorem Ipsum..."));
+        books.add(new Book("To Kill a Mockingbird", 2006, 324, "Lorem Ipsum..."));
+        books.add(new Book("Pride and Prejudice", 2000, 279, "Lorem Ipsum..."));
+        books.add(new Book("The Book Thief", 2006, 552, "Lorem Ipsum..."));
 
         books.stream()
-                .peek(book -> log.info("Saving '{}'", book.getTitle()))
-                .forEach(bookRepository::save);
+                .map(bookRepository::save)
+                .forEach(book -> LOG.info("Saved '{}' with id: {}", book.getTitle(), book.getId()));
     }
 }
